766693ea14fa78282e77e92a8dbdc6af0726eea0
yewonJin <jyw966@naver.com>
2023-04-11 20:52:34 +0900

    [UPDATE] search 페이지 초기 UI 생성

--- src/pages/search/index.tsx
+++ src/pages/search/index.tsx
@@ -1,28 +1,108 @@
+import { CommonLayout } from '@/components/Common/CommonLayout';
+import InputBox from '@/components/Common/InputBox';
+import TextBox from '@/components/Common/TextBox';
 import { getArchitects } from '@/services/api/architect';
 import Link from 'next/link';
+import { useState } from 'react';
 import { dehydrate, QueryClient, useQuery, UseQueryResult } from 'react-query';
 import styled from 'styled-components';
+import { IoIosSearch } from 'react-icons/io';
+import { Architect, participationCount, winnerCount } from '@/domain/architect';
+
+const Layout = styled.div`
+   width: 1100px;
+   margin: 0px auto;
+   padding-top: 130px;
+`;
 
 const Container = styled.ul`
+   width: 1100px;
    display: flex;
    flex-direction: column;
-   width: 1058px;
-   margin: 0px auto;
    font-size: 20px;
-   padding-top: 100px;
 
-   > li {
+   > a > li {
+      padding: 10px 25px;
+      width: 1100px;
       display: flex;
-      justify-content: center;
+      justify-content: space-around;
       align-items: center;
-      height: 50px;
+      height: 60px;
+      border-bottom: 1px solid #cacaca;
+
+      > span {
+         flex: 1;
+         font-size: 16px;
+      }
+   }
+`;
+
+const Nav = styled.nav`
+   display: flex;
+   align-items: center;
+   justify-content: space-between;
+`;
+
+const NavItem = styled.div<{ curTier: string }>`
+   display: flex;
+   align-items: center;
+   gap: 10px;
+   padding: 12px 14px;
+   border-radius: 15px;
+   background-color: ${props => (props.id === props.curTier ? '#aaa' : '')};
+   :hover {
+      cursor: pointer;
+      background-color: #aaa;
+   }
+`;
+
+const NavWrapper = styled.ul`
+   display: flex;
+   align-items: center;
+   gap: 15px;
+`;
+
+const InputWrapper = styled.div`
+   position: relative;
+   display: flex;
+   align-items: center;
+
+   > svg {
+      position: absolute;
+      left: 10px;
+      font-size: 1.5rem;
+      font-weight: 400;
+   }
+`;
+
+const LineCount = styled.span`
+   display: flex;
+   justify-content: center;
+   align-items: center;
+   padding: 5px 10px;
+   border-radius: 10px;
+   background-color: #cacaca;
+`;
+
+const TableHeader = styled.ul`
+   display: flex;
+   align-items: center;
+   justify-content: space-around;
+   width: 1100px;
+   padding: 15px 25px;
+   margin-top: 15px;
+   background-color: #ddd;
+   border-radius: 10px;
+   > li {
+      flex: 1;
+      list-style: none;
    }
 `;
 
 export const getServerSideProps = async () => {
    const queryClient = new QueryClient();
    await queryClient.prefetchQuery('architect', getArchitects);
-  
+
    return {
       props: {
          dehydratedState: dehydrate(queryClient),
@@ -30,26 +110,80 @@ export const getServerSideProps = async () => {
    };
 };
 
-type ArchitectType = {
-   wakzoo_id: string;
-   minecraft_id: string;
-   tier: string[];
-};
-
 export default function Search() {
-   const { data }: UseQueryResult<ArchitectType[]> = useQuery('architect', getArchitects);
+   const { data }: UseQueryResult<Architect[]> = useQuery('architect', getArchitects);
+   const [input, setInput] = useState('');
+   const [curTier, setCurTier] = useState('all');
 
    if (!data) return <div>no data</div>;
 
    return (
-      <Container>
-         {data.map((item, index) => {
-            return (
-               <li key={item.wakzoo_id}>
-                  <Link href={`/search/${item.minecraft_id}`}>{item.wakzoo_id + ' / ' + item.tier[0]}</Link>
-               </li>
-            );
-         })}
-      </Container>
+      <Layout>
+         <Nav>
+            <NavWrapper>
+               <NavItem id="all" curTier={curTier} onClick={() => setCurTier('all')}>
+                  <TextBox text="전체" />
+                  <LineCount>150</LineCount>
+               </NavItem>
+               <NavItem id="hacker" curTier={curTier} onClick={() => setCurTier('hacker')}>
+                  <TextBox text="해커" />
+                  <LineCount>12</LineCount>
+               </NavItem>
+               <NavItem id="gookbab" curTier={curTier} onClick={() => setCurTier('gookbab')}>
+                  <TextBox text="국밥" />
+                  <LineCount>24</LineCount>
+               </NavItem>
+               <NavItem id="pro" curTier={curTier} onClick={() => setCurTier('pro')}>
+                  <TextBox text="프로" />
+                  <LineCount>36</LineCount>
+               </NavItem>
+               <NavItem id="gaeruk" curTier={curTier} onClick={() => setCurTier('gaeruk')}>
+                  <TextBox text="계륵" />
+                  <LineCount>48</LineCount>
+               </NavItem>
+               <NavItem id="noob" curTier={curTier} onClick={() => setCurTier('noob')}>
+                  <TextBox text="눕" />
+                  <LineCount>60</LineCount>
+               </NavItem>
+            </NavWrapper>
+            <InputWrapper>
+               <IoIosSearch />
+               <InputBox
+                  type="text"
+                  name="search"
+                  value={input}
+                  onChange={e => setInput(e.target.value)}
+                  width="320px"
+                  height="40px"
+                  borderRadius="10px"
+                  border="none"
+                  backgroundColor="#F5F5F5"
+                  padding="0px 20px 0px 45px"
+               />
+            </InputWrapper>
+         </Nav>
+         <TableHeader>
+            <li>티어</li>
+            <li>마인크래프트 아이디</li>
+            <li>왁물원 아이디</li>
+            <li>참가 횟수</li>
+            <li>우승 횟수</li>
+         </TableHeader>
+         <Container>
+            {data.map((item, index) => {
+               return (
+                  <Link key={item.wakzoo_id} href={`/search/${item.minecraft_id}`}>
+                     <li>
+                        <span>{item.tier[0]}</span>
+                        <span>{item.minecraft_id}</span>
+                        <span>{item.wakzoo_id}</span>
+                        <span>{participationCount(item) + '회'}</span>
+                        <span>{winnerCount(item) + '회'}</span>
+                     </li>
+                  </Link>
+               );
+            })}
+         </Container>
+      </Layout>
    );
 }
