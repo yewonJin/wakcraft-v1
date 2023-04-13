ec69db2fd29dc87305829a233d47d9702bd311d5
yewonJin <jyw966@naver.com>
2023-04-13 22:37:47 +0900

    [UPDATE] await 추가

--- src/pages/api/architect.ts
+++ src/pages/api/architect.ts
@@ -5,7 +5,7 @@ import { NextApiRequest, NextApiResponse } from 'next';
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
    if (req.method === 'POST') {
       try {
-         Architect.create(req.body)
+         await Architect.create(req.body)
             .then(todo => res.send(todo))
             .catch(err => res.status(500).send(err));
       } catch (e) {
@@ -17,28 +17,28 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
          await connectMongo();
 
          if (req.query.tier) {
-            Architect.findAllByLineTier(req.query.tier as string)
+            await Architect.findAllByLineTier(req.query.tier as string)
                .then(architects => {
                   if (!architects.length) return res.status(404).send({ err: 'architects not found' });
                   res.json(architects);
                })
                .catch(err => res.status(500).send(err));
          } else if (req.query.search) {
-            Architect.findAllByInput(req.query.search as string)
+            await Architect.findAllByInput(req.query.search as string)
                .then(architects => {
                   if (!architects.length) return res.status(404).send({ err: 'architects not found' });
                   res.json(architects);
                })
                .catch(err => res.status(500).send(err));
          } else if (req.query.portfolio == 'false') {
-            Architect.findAllWithoutPortfolio()
+            await Architect.findAllWithoutPortfolio()
                .then(architects => {
                   if (!architects.length) return res.status(404).send({ err: 'architects not found' });
                   res.json(architects);
                })
                .catch(err => res.status(500).send(err));
          } else {
-            Architect.findAll()
+            await Architect.findAll()
                .then(architects => {
                   if (!architects.length) return res.status(404).send({ err: 'architects not found' });
                   res.json(architects);
