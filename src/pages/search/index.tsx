0013c4151b66f370e12447751876c5fc58b39e8e
yewonJin <jyw966@naver.com>
2023-04-14 20:19:43 +0900

    [UPDATE] search 페이지 티어별 건축가 수

--- src/pages/search/index.tsx
+++ src/pages/search/index.tsx
@@ -2,13 +2,14 @@ import styled from 'styled-components';
 import Link from 'next/link';
 import { GetStaticProps, InferGetStaticPropsType } from 'next';
 
-import { LineNav } from '@/components/Search/LineNav';
 import { SearchArchitect } from '@/components/Search/SearchArchitect';
-import { participationCount, winnerCount } from '@/domain/architect';
+import { createTierArray, participationCount, winnerCount } from '@/domain/architect';
 import connectMongo from '@/utils/connectMongo';
 import Architect from '@/models/architect';
 import { convertLineTierToTier } from '@/controller/architect';
 import { useShowArchitect } from '@/application/showArchitect';
+import TextBox from '@/components/Common/TextBox';
+import { translateTier } from '@/utils/lib';
 
 const Layout = styled.div`
    width: 1000px;
@@ -37,7 +38,7 @@ const TableHeader = styled.ul`
    }
 `;
 
-const List = styled.ul`
+const ArchitectList = styled.ul`
    width: 1000px;
    display: flex;
    flex-direction: column;
@@ -58,10 +59,38 @@ const List = styled.ul`
    }
 `;
 
+const TierList = styled.ul`
+   display: flex;
+   align-items: center;
+   gap: 15px;
+`;
+
+const NavItem = styled.div<{ curTier: string }>`
+   display: flex;
+   align-items: center;
+   gap: 10px;
+   padding: 12px 14px;
+   border-radius: 15px;
+   background-color: ${props => (props.id === props.curTier ? '#aaa' : '')};
+   :hover {
+      cursor: pointer;
+      background-color: #aaa;
+   }
+`;
+
+const LineCount = styled.span`
+   display: flex;
+   justify-content: center;
+   align-items: center;
+   padding: 5px 10px;
+   border-radius: 10px;
+   background-color: #cacaca;
+`;
+
 export const getStaticProps: GetStaticProps<{ architects: Architect[] }> = async context => {
    await connectMongo();
 
-   const architects = await Architect.findAll();
+   const architects = await Architect.findAllWithoutPortfolio();
 
    return {
       props: {
@@ -72,22 +101,31 @@ export const getStaticProps: GetStaticProps<{ architects: Architect[] }> = async
 };
 
 export default function Search({ architects }: InferGetStaticPropsType<typeof getStaticProps>) {
-   const { curTier } = useShowArchitect();
+   const { curTier, setNavCurrentTier } = useShowArchitect();
 
    return (
       <Layout>
          <Nav>
-            <LineNav />
+            <TierList>
+               {['hacker', 'gukbap', 'pro', 'gyeruik', 'noob'].map(tier => (
+                  <NavItem id={tier} key={tier} curTier={curTier} onClick={() => setNavCurrentTier(tier)}>
+                     <TextBox text={translateTier(tier)} />
+                     <LineCount>
+                        {architects.filter(item => convertLineTierToTier(tier).includes(item.tier[0])).length}
+                     </LineCount>
+                  </NavItem>
+               ))}
+            </TierList>
             <SearchArchitect />
          </Nav>
          <TableHeader>
             <li>티어</li>
             <li>마인크래프트 아이디</li>
             <li>왁물원 아이디</li>
-            <li>참가 횟수</li>
+            <li>참여 횟수</li>
             <li>우승 횟수</li>
          </TableHeader>
-         <List>
+         <ArchitectList>
             {architects
                .filter(item => convertLineTierToTier(curTier).includes(item.tier[0]))
                .map((item, _) => {
@@ -97,13 +135,13 @@ export default function Search({ architects }: InferGetStaticPropsType<typeof ge
                            <span>{item.tier[0]}</span>
                            <span>{item.minecraft_id}</span>
                            <span>{item.wakzoo_id}</span>
-                           <span>{participationCount(item) + '회'}</span>
-                           <span>{winnerCount(item) + '회'}</span>
+                           <span>{item.noobProHackerInfo.participation + '회'}</span>
+                           <span>{item.noobProHackerInfo.win + '회'}</span>
                         </li>
                      </Link>
                   );
                })}
-         </List>
+         </ArchitectList>
       </Layout>
    );
 }
