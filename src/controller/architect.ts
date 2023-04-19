df0665fd860bdcc35fb0d70158026d56b7e1bf16
yewonJin <jyw966@naver.com>
2023-04-19 14:41:06 +0900

    [UPDATE] clean up

--- src/controller/architect.ts
+++ src/controller/architect.ts
@@ -1,25 +1,9 @@
-const tier = [
-   '마카게',
-   '오마카세',
-   '해커',
-   '해장국',
-   '국밥',
-   '미지근한 국밥',
-   '식은 국밥',
-   '프로',
-   '계추',
-   '계륵',
-   '착한 눕',
-   '안 나쁜 눕',
-   '그냥 눕',
-   '진짜 눕',
-   '언랭',
-];
+import { createTierArray } from '@/domain/architect';
 
 export const convertLineTierToTier = (req: string) => {
-   if (req === 'hacker') return tier.slice(0, 4);
-   else if (req === 'gukbap') return tier.slice(4, 7);
-   else if (req === 'pro') return tier.slice(7, 8);
-   else if (req === 'gyeruik') return tier.slice(8, 10);
-   else return tier.slice(10);
+   if (req === 'hacker') return createTierArray().slice(0, 4);
+   else if (req === 'gukbap') return createTierArray().slice(4, 7);
+   else if (req === 'pro') return createTierArray().slice(7, 8);
+   else if (req === 'gyeruik') return createTierArray().slice(8, 10);
+   else return createTierArray().slice(10);
 };
