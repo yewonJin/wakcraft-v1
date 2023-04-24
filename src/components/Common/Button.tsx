42019a3413839ddafa26d082f4ad4f1d4407be52
yewonJin <jyw966@naver.com>
2023-04-25 01:32:56 +0900

    [UPDATE] Common Button 생성

---
+++ src/components/Common/Button.tsx
@@ -0,0 +1,43 @@
+import styled from 'styled-components';
+
+type ButtonStyleType = {
+   border?: string;
+   borderRadius?: string;
+   backgroundColor?: string;
+   color?: string;
+   padding?: string;
+   fontSize?: string;
+   hoverBackgroundColor?: string;
+   hoverColor?: string;
+};
+
+const ButtonBox = styled.button<ButtonStyleType>`
+   border: ${props => props.border || 'none'};
+   border-radius: ${props => props.borderRadius || '5px'};
+   background-color: ${props => props.backgroundColor || '#4caf50'};
+   color: ${props => props.color || 'white'};
+   padding: ${props => props.padding || '12px 0px'};
+   font-size: ${props => props.fontSize || '16px'};
+
+   :hover {
+      cursor: pointer;
+      background-color: ${props => props.hoverBackgroundColor || '#3d9c40'};
+      color: ${props => props.hoverColor || ''};
+   }
+`;
+
+type ButtonType = {
+   text: string;
+   onClick?: (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => void;
+   [props: string]: any;
+};
+
+export function Button(props: ButtonType) {
+   const { onClick, text, ...restProps } = props;
+
+   return (
+      <ButtonBox onClick={onClick} {...restProps}>
+         {text}
+      </ButtonBox>
+   );
+}
