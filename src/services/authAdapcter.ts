6aad3ce2e0d8e6c7f561a941373107bf4df51d87
yewonJin <jyw966@naver.com>
2023-04-24 20:18:43 +0900

    [UPDATE] react-hot-toast를 이용한 에러 처리 테스트

--- src/services/authAdapcter.ts
+++ src/services/authAdapcter.ts
@@ -4,14 +4,13 @@ import toast from 'react-hot-toast';
 import { login } from './api/auth';
 
 export const useMutationLogin = () => {
-   const { isLoading, mutate } = useMutation((body: any) => login(body), {
-      onError(err: { message: string }) {
-         toast.error(err.message);
-      },
-      onSuccess() {
-         toast.success('로그인');
-      },
-   });
+   const { isLoading, mutate } = useMutation((body: any) =>
+      toast.promise(login(body), {
+         loading: '로그인 중',
+         success: '로그인 성공',
+         error: err => err.response.data,
+      }),
+   );
 
    return { isLoading, mutate };
 };
