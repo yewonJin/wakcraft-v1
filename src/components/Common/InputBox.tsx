43db6b75d80138304d341a7b0bd5c9b9311c2eca
yewonJin <jyw966@naver.com>
2023-05-01 15:47:33 +0900

    [STYLE] type='file'인 input 스타일 변경

--- src/components/Common/InputBox.tsx
+++ src/components/Common/InputBox.tsx
@@ -11,7 +11,7 @@ type InputStyleType = {
    padding?: string;
    fontSize?: string;
    lineHeight?: string;
-   backgroundColor?: string
+   backgroundColor?: string;
 };
 
 const Input = styled.input<InputStyleType>`
@@ -24,8 +24,32 @@ const Input = styled.input<InputStyleType>`
    font-size: ${props => props.fontSize || '16px'};
    line-height: ${props => props.lineHeight || '24px'};
    padding: ${props => props.padding || '0px 5px'};
+   color: ${props => props.color || '#999'};
    background-color: ${props => props.backgroundColor || ''};
    outline: none;
+`;
+
+const FileBox = styled.div`
+   display: flex;
+
+   > input[type='file'] {
+      position: absolute;
+      width: 0;
+      height: 0;
+      padding: 0;
+      overflow: hidden;
+      border: 0;
+   }
+`;
+
+const Label = styled.label`
+   display: inline-block;
+   padding: 5px 15px;
+   color: #fff;
+   vertical-align: middle;
+   background-color: #777;
+   cursor: pointer;
+   height: 35px;
 `;
 
 type InputBoxType = {
@@ -39,6 +63,14 @@ type InputBoxType = {
 
 function InputBox(props: InputBoxType) {
    const { type, placeholder, value, onChange, name, ...restProps } = props;
+
+   if (type === 'file')
+      return (
+         <FileBox>
+            <Label htmlFor="file">파일 업로드</Label>
+            <Input id="file" type={type} onChange={onChange} {...restProps}></Input>
+         </FileBox>
+      );
 
    return <Input type={type} placeholder={placeholder} name={name} value={value} onChange={onChange} {...restProps} />;
 }
