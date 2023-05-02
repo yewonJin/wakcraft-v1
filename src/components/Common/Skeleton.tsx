e8dc2d221b7517fec820233e1c67b3defd820a4a
yewonJin <jyw966@naver.com>
2023-05-02 15:40:15 +0900

    [UPDATE] 건축가 세부사항 Skeleton Loading 기능

---
+++ src/components/Common/Skeleton.tsx
@@ -0,0 +1,52 @@
+import styled, { keyframes } from 'styled-components';
+
+type SkeletonType = {
+   [props: string]: string | boolean;
+};
+
+type SkeletonStyleType = {
+   backgroundColor?: string;
+   width?: string;
+   height?: string;
+   margin?: string;
+   padding?: string;
+   borderRadius?: string;
+};
+
+const loading = keyframes`
+    0% {
+         transform: translateX(0px);
+    }
+    50%,
+    100% {
+         transform: translateX(200px);
+    }
+`;
+
+const Span = styled.span<SkeletonStyleType>`
+   position: absolute;
+   width: 10%;
+   height: 100%;
+   background-color: #d3d3d3;
+   z-index: 2;
+   animation: ${loading} 1.5s infinite linear;
+`;
+
+const Layout = styled.div<SkeletonStyleType>`
+   position: relative;
+   background-color: ${props => props.backgroundColor || '#ddd'};
+   width: ${props => props.width || 'auto'};
+   height: ${props => props.height || 'auto'};
+   border-radius: ${props => props.borderRadius || '0px'};
+   overflow: hidden;
+`;
+
+export default function Skeleton(props: SkeletonType) {
+   const { ...restProps } = props;
+
+   return (
+      <Layout {...restProps}>
+         <Span></Span>
+      </Layout>
+   );
+}
--- src/pages/architect/[id].tsx
+++ src/pages/architect/[id].tsx
@@ -5,6 +5,7 @@ import { useQueryArchitectById } from '@/services/architectAdapters';
 import { CommonLayout } from '@/components/Common/CommonLayout';
 import TextBox from '@/components/Common/TextBox';
 import icon from '../../../public/1.png';
+import Skeleton from '@/components/Common/Skeleton';
 
 const ProfileBox = styled.div`
    display: flex;
@@ -21,10 +22,32 @@ const ProfileImage = styled.span`
    background-size: cover;
 `;
 
+const SkeletonBox = styled.div`
+   display: grid;
+   grid-template-columns: repeat(3, minmax(300px, 1fr));
+   margin-top: 20px;
+   gap: 30px;
+   row-gap: 50px;
+`;
+
 export default function Page() {
    const data = useQueryArchitectById();
 
-   if (!data) return <CommonLayout>로딩중...</CommonLayout>;
+   if (!data)
+      return (
+         <CommonLayout>
+            <ProfileBox>
+               <Skeleton width="70px" height="70px" borderRadius="50px" />
+               <Skeleton width="130px" height="30px" />
+            </ProfileBox>
+            <Skeleton width="130px" height="30px" margin="5px 0px 0px 0px" />
+            <SkeletonBox>
+               {[...new Array(9).fill(0)].map((_, index) => (
+                  <Skeleton key={'Skeleton' + index} width="380px" height="213px" borderRadius="15px" />
+               ))}
+            </SkeletonBox>
+         </CommonLayout>
+      );
 
    console.log(data);
 
