1fd466305f29f5447cf2da6c6bfda43fa99ec0da
yewonJin <jyw966@naver.com>
2023-06-02 19:23:19 +0900

    [UPDATE] 월드컵 domain, models 생성

---
+++ src/domain/worldcup.ts
@@ -0,0 +1,12 @@
+export type Worldcup = {
+   subject: 'HackerWorldCup';
+   workInfo: {
+      minecraft_id: string;
+      episode: number;
+      subject: string;
+      image_url: string;
+      youtube_url: string;
+   };
+   numberOfWin: number;
+   numberOfParticipation: number;
+};
---
+++ src/models/worldcup.ts
@@ -0,0 +1,32 @@
+import { Worldcup } from '@/domain/worldcup';
+import { Model, Schema, model, models } from 'mongoose';
+
+interface WorldcupModel extends Model<Worldcup> {
+   findAllBySubject: (payload: Worldcup) => Promise<void>;
+}
+
+const worldcupSchema = new Schema<Worldcup, WorldcupModel>({
+   subject: { type: String },
+   workInfo: {
+      minecraft_id: { type: String },
+      episode: { type: Number },
+      subject: { type: String },
+      image_url: { type: String },
+      youtube_url: { type: String },
+   },
+   numberOfWin: { type: Number },
+   numberOfParticipation: { type: Number },
+});
+
+worldcupSchema.statics.create = function (payload) {
+   const worldcup = new this(payload);
+   return worldcup.save();
+};
+
+worldcupSchema.statics.findAllBySubject = function (subject: string) {
+   return this.find({ subject: subject });
+};
+
+const Worldcup = (models['Worldcup'] as WorldcupModel) || model<Worldcup, WorldcupModel>('Admin', worldcupSchema);
+
+export default Worldcup;
