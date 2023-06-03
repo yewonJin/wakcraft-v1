53c830cb940a4d0446504ca00933ba2a229ab6c7
yewonJin <jyw966@naver.com>
2023-06-03 21:04:50 +0900

    [UPDATE] 우승 작품 - 정보 보여주기

--- src/components/WorldCup/Winner.tsx
+++ src/components/WorldCup/Winner.tsx
@@ -3,10 +3,11 @@ import styled from 'styled-components';
 
 import { renameTo1080Webp } from '@/domain/noobProHacker';
 import { Worldcup } from '@/domain/worldcup';
+import ImageInfo from '../Common/ImageInfo';
 
 const ImageBox = styled.div`
    position: relative;
-   width: 1200px;
+   width: 1400px;
    margin: 0px auto;
    aspect-ratio: 16/9;
    transition-duration: 200ms;
@@ -28,6 +29,12 @@ export default function Winner({ curRoundArr }: { curRoundArr: Worldcup[] }) {
             fill
             alt="NoobProHacker Image"
             src={renameTo1080Webp(curRoundArr[0].workInfo.image_url)}
+         />
+         <ImageInfo
+            episode={curRoundArr[0].workInfo.episode}
+            subject={curRoundArr[0].workInfo.subject}
+            minecraft_id={curRoundArr[0].workInfo.minecraft_id}
+            onClick={() => window.open(curRoundArr[0].workInfo.youtube_url)}
          />
       </ImageBox>
    );
